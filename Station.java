6c14623842f121b22c98607a49c1691ce55ffe27
Warren Peng <wpeng26@students.polytechnic.org>
2024-02-28 01:02:48 -0800

    finish station

--- Station.java
+++ Station.java
@@ -22,6 +22,104 @@ public class Station{
     }
 
     public void boardTrain(){
-        for(int i = people.size()
+        for (int n =people.size() - 1; n >= 0; n--){
+            Passenger person = people.get(n);
+            if (person.getDestination() > stationNumber)
+            {
+                int carIndex = -1;
+                for (int i = 0; i < cars.size(); i++)
+                {
+                    Car car = cars.get(i);
+                    if (car.getDestination() > stationNumber)
+                    {
+                        carIndex = i;
+                    }
+                }
+        if (carIndex != -1 && cars.get(carIndex).getPersons().size() != 3)
+                {
+                    cars.get(carIndex).addPerson(person);
+                    people.remove(j);
+                }
+            }
+            else if (person.getDestination() < stationNumber)
+            {
+                int carIndex = -1;
+                for (int i = 0; i < cars.size(); i++)
+                {
+                    Car car = cars.get(i);
+                    if (car.getDestination() < stationNumber)
+                    {
+                        carIndex = i;
+                    }
+                }
+                if (carIndex != -1 && cars.get(carIndex).getPersons().size() != 3)
+                {
+                    cars.get(carIndex).addPerson(person);
+                    people.remove(j);
+                }
+            }
+        }
+    }
+    public void unloadPassengers()
+    {
+        for (int i = 0; i < cars.size(); i++)
+        {
+            Car car = cars.get(i);
+            for (int n = car.getPersons().size() - 1; n > -1; n--)
+            {
+                if (car.getPersons().get(n).getDestination() == getStationNumber())
+                {
+                    car.removePerson(n);
+                }
+            }
+        }
+    }
+    public void checkCars()
+    {
+        for (int i = cars.size() - 1; i > -1; i--)
+        {
+            if (cars.get(i).getDestination() == getStationNumber())
+            {
+                despawnCar(i);
+            }
+        }
+    }
+    public void spawnPerson(int destination)
+    {
+        people.add(new Passenger(destination));
+    }
+    public void addPerson(Passenger person)
+    {
+        people.add(person);
+    }
+    public void removePerson(int index)
+    {
+        people.remove(index);
+    }
+    public ArrayList<Passenger> getPersons()
+    {
+        return people;
+    }
+    public void spawnCar(int destination)
+    {
+        cars.add(new Car(stationNumber, destination));
+    }
+    public void addCar(Car car)
+    {
+        cars.add(car);
+    }    
+    public void despawnCar(int index)
+    {
+        Car car = cars.get(index);
+        for (int i = car.getPersons().size() - 1; i > -1; i--)
+        {
+            addPerson(car.getPersons().get(i));
+            car.removePerson(i);
+        }
+        cars.remove(index);
+    }
+    public void removeCar(int index)
+    {
+        cars.remove(index);
     }
 }
