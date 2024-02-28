7d5cab17547d8b9bdbbacaee8947a639408ebf12
Warren Peng <wpeng26@students.polytechnic.org>
2024-02-27 23:52:43 -0800

    mutator

--- Car.java
+++ Car.java
@@ -2,9 +2,32 @@ import java.util.ArrayList;
 
 public class Car {
     //fields
-    private ArrayList<Passenger> persons;
+    private ArrayList<Passenger> people;
     private int destination;
     private int initialStation;
     private int currentStation;
     private boolean moveable;
+
+    //constructors
+    public Car(int myInitialStation, int myDestination){
+        destination = myDestination;
+        initialStation = myInitialStation;
+    }
+
+    //mutators
+    public void addPerson(Passenger human){
+        if(people.size() < 3){
+            people.add(human);
+        }else{
+        }
+    }
+
+    public void removePerson(int index) {
+        people.remove(index);
+    }
+
+    public void moveStations(){
+        currentStation++;
+    }
+
 }
