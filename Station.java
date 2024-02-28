4f0baaa5460c573a729a527bca1eb37384e42375
Warren Peng <wpeng26@students.polytechnic.org>
2024-02-28 00:09:05 -0800

    more getters

--- Station.java
+++ Station.java
@@ -8,11 +8,20 @@ public class Station{
     private ArrayList<Car> cars;
     private int stationNumber;
 
-    //
-    public Station(int myStationNumber)
-    {
+    //constructor
+    public Station(int myStationNumber){
         people = new ArrayList<Passenger>();
         cars = new ArrayList<Car>();
         stationNumber = myStationNumber;
+    }
+    public ArrayList<Car> getCars(){
+        return cars;
+    }
+    public int getStationNumber(){
+        return stationNumber;
+    }
+
+    public void boardTrain(){
+        for(int i = people.size()
     }
 }
