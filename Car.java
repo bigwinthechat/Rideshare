149e1827a279032cb4b8327a2eb900957fe884b4
Warren Peng <wpeng26@students.polytechnic.org>
2024-02-27 23:32:23 -0800

    fields

--- Car.java
+++ Car.java
@@ -1,3 +1,10 @@
+import java.util.ArrayList;
+
 public class Car {
-    
+    //fields
+    private ArrayList<Passenger> persons;
+    private int destination;
+    private int initialStation;
+    private int currentStation;
+    private boolean moveable;
 }
